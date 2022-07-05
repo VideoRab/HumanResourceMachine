3a81c628110b2163e408cfa668a83b02249dbaf8
Ilya Safonov <illia220011@gmail.com>
2022-07-05 11:57:15 +0300

    Added folder "Entities" and created Entity.

---
+++ HumanResourceMachine/Entities/Human.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanResourceMachine.Entities
+{
+    public class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
