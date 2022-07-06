bcb73b53e522f2b75a895655c3c82df896abafa9
Ilya Safonov <illia.2001@mail.ru>
2022-07-06 12:01:16 +0300

    Implemented EFCore. (#4)
    Co-authored-by: Ilya Safonov <illia220011@gmail.com>

---
+++ HumanResourceMachine/Context/HRMContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HumanResourceMachine.Entities;
+
+namespace HumanResourceMachine.Context
+{
+    public class HRMContext : DbContext
+    {
+        public DbSet<Human> People { get; set; } = null!;
+        public HRMContext(DbContextOptions<HRMContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Human>().HasData(
+                new Human { Id = 1, Name = "Ilya", Surname = "Safonov", Patronymic = "Alexandrovich" },
+                new Human { Id = 2, Name = "SomeName", Surname = "SomeSurname", Patronymic = "SomePatronymic" }
+            );
+        }
+    }
+}
