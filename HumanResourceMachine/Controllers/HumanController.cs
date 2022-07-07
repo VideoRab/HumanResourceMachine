02e95f94cf42f5843f96f65d8e8b1ac122f405cc
Ilya Safonov <illia220011@gmail.com>
2022-07-07 12:42:11 +0300

    Added controller with CRUD operations for entity. (#3)
    * Added controller with CRUD operations for entity.
    
    * Added controller with CRUD operations for entity.
    
    * Revert "Added controller with CRUD operations for entity."
    
    This reverts commit 82175cb7d974c3c10d685362ccf70af2c4d7871a.
    
    * Renamed methods and changed their response.
    
    * Added controller with CRUD operations for entity.
    
    * Added db to controller and remove IActionResult.
    
    * Added [FromRoute] and renamed PostNewHuman method.
    
    * Renamed methods.
    
    * Change put method.
    
    * Delete async code. Now methods return only objects.
    
    * Changed methods naming.
    
    * Rename DeletById method.

---
+++ HumanResourceMachine/Controllers/HumanController.cs
@@ -0,0 +1,57 @@
+﻿using HumanResourceMachine.Context;
+using HumanResourceMachine.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HumanResourceMachine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HumanController : ControllerBase
+    {
+        private readonly HRMContext _context;
+
+        public HumanController(HRMContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Human> GetAllHumans()
+        {
+            return _context.People.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Human GetHumanById([FromRoute] int id)
+        {
+            var result = _context.People.Find(id);
+
+            return result;
+        }
+
+        [HttpPost]
+        public void AddHuman(Human human)
+        {
+            _context.People.Add(human);
+            _context.SaveChanges();
+        }
+
+        [HttpPut]
+        public void UpdateHuman(Human human)    
+        {
+            _context.Update(human); 
+            _context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteHumanById([FromRoute] int id)
+        {
+            var target = _context.People.Find(id);
+            _context.People.Remove(target); 
+            _context.SaveChanges();
+        }
+    }
+}
