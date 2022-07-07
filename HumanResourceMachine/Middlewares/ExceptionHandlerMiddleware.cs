3fdf4e11b566988577097f1623053e92898b5ab4
Ilya Safonov <illia220011@gmail.com>
2022-07-07 16:17:33 +0300

    Created ExceptionHandlerMiddleware and deleted weatherforecast. (#6)
    * Created ExceptionHandlerMiddleware and deleted weatherforecast.
    
    * Added registration of middleware.

---
+++ HumanResourceMachine/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace HumanResourceMachine.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionMessageAsync(context.Response, ex);
+            }
+        }
+
+        private async Task HandleExceptionMessageAsync(HttpResponse response, Exception ex)
+        {
+            response.ContentType = "application/json";
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var message = JsonSerializer.Serialize(new
+            {
+                StatusCode = response.StatusCode,
+                ErrorMessage = ex.Message
+            });
+
+            await response.WriteAsync(message);
+        }
+    }
+}
