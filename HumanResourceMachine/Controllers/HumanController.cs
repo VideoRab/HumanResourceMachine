e39aa3c97e550b82840020947cb9daa9e3b22c8a
Ilya Safonov <illia220011@gmail.com>
2022-07-08 13:58:12 +0300

    Renamed bls class and interface. Changed di lifetime.

--- HumanResourceMachine/Controllers/HumanController.cs
+++ HumanResourceMachine/Controllers/HumanController.cs
@@ -1,5 +1,5 @@
-﻿using HumanResourceMachine.Context;
-using HumanResourceMachine.Entities;
+﻿using HumanResourceMachine.Entities;
+using HumanResourceMachine.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,56 +10,41 @@ namespace HumanResourceMachine.Controllers
     [ApiController]
     public class HumanController : ControllerBase
     {
-        private readonly HRMContext _context;
+        private readonly IHumanService _service;
 
-        public HumanController(HRMContext context)
+        public HumanController(IHumanService service)
         {
-            _context = context;
+            _service = service;
         }
 
         [HttpGet]
         public IEnumerable<Human> GetAllHumans()
         {
-            return _context.People.ToList();
+            return _service.GetAllHumans();
         }
 
         [HttpGet("{id}")]
         public Human GetHumanById([FromRoute] int id)
         {
-            var result = _context.People.Find(id);
-            if (result is null)
-            {
-                throw new ArgumentNullException("Object doesn't exist.", nameof(result));
-            }
-
-            return result;
+            return _service.GetHumanById(id);
         }
 
         [HttpPost]
         public void AddHuman(Human human)
         {
-            _context.People.Add(human);
-            _context.SaveChanges();
+            _service.AddHuman(human);
         }
 
         [HttpPut]
-        public void UpdateHuman(Human human)    
+        public void UpdateHuman(Human human)
         {
-            _context.Update(human); 
-            _context.SaveChanges();
+            _service.UpdateHuman(human);
         }
 
         [HttpDelete("{id}")]
         public void DeleteHumanById([FromRoute] int id)
         {
-            var target = _context.People.Find(id);
-            if (target is null)
-            {
-                throw new ArgumentNullException("Object doesn't exist.", nameof(target));
-            }
-
-            _context.People.Remove(target); 
-            _context.SaveChanges();
+            _service.DeleteHumanById(id);
         }
     }
 }
---
+++ HumanResourceMachine/Services/HumanService.cs
@@ -0,0 +1,57 @@
+﻿using HumanResourceMachine.Context;
+using HumanResourceMachine.Entities;
+using HumanResourceMachine.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanResourceMachine.Services
+{
+    public class HumanService : IHumanService
+    {
+        private readonly HRMContext _context;
+
+        public HumanService(HRMContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Human> GetAllHumans()
+        {
+            return _context.People.ToList();
+        }
+
+        public Human GetHumanById(int id)
+        {
+            var result = _context.People.Find(id);
+            if (result is null)
+            {
+                throw new ArgumentNullException("Object doesn't exist.", nameof(result));
+            }
+
+            return result;
+        }
+
+        public void AddHuman(Human human)
+        {
+            _context.People.Add(human);
+            _context.SaveChanges();
+        }
+
+        public void UpdateHuman(Human human)
+        {
+            _context.Entry(human).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteHumanById(int id)
+        {
+            var target = _context.People.Find(id);
+            if (target is null)
+            {
+                throw new ArgumentNullException("Object doesn't exist.", nameof(target));
+            }
+
+            _context.People.Remove(target);
+            _context.SaveChanges();
+        }
+    }
+}
