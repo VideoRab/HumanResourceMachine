2aaa281aceb27b40a9ff6fa5cf597eade183a384
Ilya Safonov <illia220011@gmail.com>
2022-07-12 10:10:26 +0300

    Added model based on entity.

---
+++ HumanResourceMachine/Models/HumanModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanResourceMachine.Models
+{
+    public class HumanModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
