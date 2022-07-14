23bf3080ec003857f09da02660ceadcc6c2c6aa9
Ilya Safonov <illia220011@gmail.com>
2022-07-14 17:22:24 +0300

    Added model and viewmodel for new entity. (#19)
    * Added model and viewmodel for new entity.
    
    * Remove useless usings.

---
+++ HRM.BLL/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace HRM.BLL.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public List<Human> Employees { get; set; }
+    }
+}
---
+++ HumanResourceMachine/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HumanResourceMachine.ViewModels
+{
+    public class CompanyViewModel
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public List<HumanViewModel> Employees { get; set; }
+    }
+}
