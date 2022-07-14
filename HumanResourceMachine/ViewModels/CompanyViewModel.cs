ebb6565cd92f144369c7271b58d71c543f9ad021
Ilya Safonov <illia220011@gmail.com>
2022-07-14 16:56:04 +0300

    Added model and viewmodel for new entity.

---
+++ HumanResourceMachine/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using HRM.BLL.Models;
+
+namespace HumanResourceMachine.ViewModels
+{
+    public class CompanyViewModel
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public List<HumanViewModel> Employees { get; set; }
+    }
+}
