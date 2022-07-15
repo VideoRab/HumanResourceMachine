6f96b7fc171b01924e1a6775868cfbabe8c6aa7f
Ilya Safonov <illia220011@gmail.com>
2022-07-15 11:58:07 +0300

    Added new entity using migration. (#18)
    * Added new entity using migration.
    
    * Rename Company class.

---
+++ HRM.DAL/Migrations/20220714130933_Company.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRM.DAL.Migrations
+{
+    public partial class Company : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "People",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_CompanyId",
+                table: "People",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_People_Companies_CompanyId",
+                table: "People",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_People_Companies_CompanyId",
+                table: "People");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_People_CompanyId",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "People");
+        }
+    }
+}
