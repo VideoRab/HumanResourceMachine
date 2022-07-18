3a92c15e266f729d2df6ae04e591de7da316a3ac
Ilya Safonov <illia220011@gmail.com>
2022-07-19 00:19:12 +0300

    Added generic controller.

---
+++ HumanResourceMachine/Controllers/GenericController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using HRM.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResourceMachine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenericController<TViewModel, TModel> : ControllerBase
+        where TViewModel : class
+        where TModel : class
+    {
+        private readonly IGenericService<TModel> _service;
+        private readonly IMapper _mapper;
+
+        public GenericController(IGenericService<TModel> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<TViewModel>> GetAll(CancellationToken token)
+        {
+            var tModels = await _service.GetAll(token);
+            var result = _mapper.Map<IEnumerable<TViewModel>>(tModels);
+
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<TViewModel> GetById([FromRoute] int id, CancellationToken token)
+        {
+            var tModel = await _service.GetById(id, token);
+            var result = _mapper.Map<TViewModel>(tModel);
+
+            return result;
+        }
+
+        [HttpPost]
+        public async Task Add(TViewModel tViewModel, CancellationToken token)
+        {
+            var tModel = _mapper.Map<TModel>(tViewModel);
+            await _service.Add(tModel, token);
+        }
+
+        [HttpPut]
+        public async Task Update(TViewModel tViewModel, CancellationToken token)
+        {
+            var tModel = _mapper.Map<TModel>(tViewModel);
+            await _service.Update(tModel, token);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteById([FromRoute] int id, CancellationToken token)
+        {
+            await _service.DeleteById(id, token);
+        }
+    }
+}
