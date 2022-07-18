3f0759b052adb23fabf881c0f335433063bce09e
Ilya Safonov <illia220011@gmail.com>
2022-07-19 00:12:08 +0300

    Added generic repository.

---
+++ HRM.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HRM.DAL.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+        where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll(CancellationToken token);
+        Task<TEntity> GetById(int id, CancellationToken token);
+        Task Add(TEntity tEntity, CancellationToken token);
+        Task Update(TEntity tEntity, CancellationToken token);
+        Task DeleteById(int id, CancellationToken token);
+    }
+}
