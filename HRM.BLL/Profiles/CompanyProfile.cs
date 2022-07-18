e8a99e396f0cfb2fe80464cc399b2847e4454765
Ilya Safonov <illia220011@gmail.com>
2022-07-19 00:18:50 +0300

    Added generic service.

---
+++ HRM.BLL/Profiles/CompanyProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HRM.BLL.Models;
+using HRM.DAL.Entities;
+
+namespace HRM.BLL.Profiles
+{
+    public class CompanyProfile : Profile
+    {
+        public CompanyProfile()
+        {
+            CreateMap<Company, CompanyEntity>().ReverseMap();
+        }
+    }
+}
